68382d3a2421df4cf01b5b5ca449a6ce0f64a6fc
Anant Prakash Awasthi <anant.awasthi@sscr.co.in>
2020-07-12 19:31:35 +0530

    Connect and Data Exchange with MS-SQL
    The script includes connection with MS SQL Server with SQL Authentication, create table and reading from SQL.

---
+++ Database/Read_Write_with_MSSQL.py
@@ -0,0 +1,27 @@
+
+# Import Libraries
+import pyodbc
+import pandas as pd
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+
+# Share Database Details
+database_server = 'database_server'
+username = 'username'
+password = 'password'
+database_name = 'database_name'
+
+# Create an SQL Engine
+engine = sa.create_engine('mssql+pyodbc://' + username + ':' + password + '@' + database_server + '/' + database_name+'?DRIVER={SQL Server Native Client 11.0}')
+
+# Reading Data to upload to Server
+rdata = pd.read_excel('http://d3analytics.co.in/datasets/athelete_dataset/Athelete_Data.xlsx')
+
+# Writing Data to SQL Server
+rdata.to_sql('CURD_Python', con = engine, if_exists = 'replace')
+
+# Reading Data from SQL Server
+sql = 'select count(*) from table'
+data = pd.read_sql(sql)
+
+# *************************************************************************************

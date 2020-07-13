cc3ccc7784da5875048ae22659d18fe7939c4dc7
Anant Prakash Awasthi <anant.awasthi@sscr.co.in>
2020-07-13 15:27:54 +0530

    Updated Table name as an argument

--- Database/Read_Write_with_MSSQL.py
+++ Database/Read_Write_with_MSSQL.py
@@ -10,6 +10,7 @@ database_server = 'database_server'
 username = 'username'
 password = 'password'
 database_name = 'database_name'
+table_name = 'table_name'
 
 # Create an SQL Engine
 engine = sa.create_engine('mssql+pyodbc://' + username + ':' + password + '@' + database_server + '/' + database_name+'?DRIVER={SQL Server Native Client 11.0}')
@@ -18,7 +19,7 @@ engine = sa.create_engine('mssql+pyodbc://' + username + ':' + password + '@' + 
 rdata = pd.read_excel('http://d3analytics.co.in/datasets/athelete_dataset/Athelete_Data.xlsx')
 
 # Writing Data to SQL Server
-rdata.to_sql('CURD_Python', con = engine, if_exists = 'replace')
+rdata.to_sql(table_name, con = engine, if_exists = 'replace')
 
 # Reading Data from SQL Server
 sql = 'select count(*) from table'

a5b940e14daa70401d25d1f012981bba43921e32
Anant Prakash Awasthi <anant.awasthi@sscr.co.in>
2020-08-12 23:14:17 +0530

    Updated Codes

--- Cloud_Computing/AWS/An_Introduction_AmazonS3_with_Python.py
+++ Cloud_Computing/AWS/An_Introduction_AmazonS3_with_Python.py
@@ -1,11 +1,22 @@
-# Reference : https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+#!/usr/bin/env python
+# coding: utf-8
+
+# Reference : https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html 
 
 # #### Activities in this module:
+#     
+
+# In[ ]:
+
 
 # Installing Libraries
- pip install boto3
- pip install pandas
- pip install os
+# pip install boto3
+# pip install pandas
+# pip install os
+
+
+# In[ ]:
+
 
 # Loading Libraries
 import boto3
@@ -15,22 +26,40 @@ import os
 
 # #### AWS Authentication
 
+# In[ ]:
+
+
 # S3 Credentials
 AccesskeyID = 'xxxx'
 Secretaccesskey = 'xxxx'
+
+
+# In[ ]:
+
+
 s3 = boto3.client("s3", 
                   aws_access_key_id=AccesskeyID, 
                   aws_secret_access_key=Secretaccesskey)
 
 
 # #### Creating a bucket
-bucket = s3.create_bucket(Bucket= 'sgsanalytics')
+
+# In[ ]:
+
+
+bucket = s3.create_bucket(Bucket= '<Bucket_Name>')
+
 
 # #### List of Existing Bucket
+
+# In[ ]:
+
+
 response = s3.list_buckets()
 
-# #### List Bucket Content
-s3.list_objects(Bucket='sgsanalytics')['Contents']
+
+# In[ ]:
+
 
 #Output the bucket names
 print('Existing buckets:')
@@ -38,16 +67,48 @@ for bucket in response['Buckets']:
     print(f'  {bucket["Name"]}')
 
 
+# #### List Bucket Content
+
+# In[ ]:
+
+
+s3.list_objects(Bucket='<Bucket_Name>')['Contents']
+
+
 # #### Upload a File
-file_path = r"C:\Users\Anant Awasthi\Google Drive\SSCR India\sscr.ai\Stakeholders.xlsx"
-s3.upload_file(file_path, 'sgsanalytics', 'Dir1/Stakeholders.xlsx')
+
+# In[ ]:
+
+
+file_path = r"YourFilePath"
+
+
+# In[ ]:
+
+
+s3.upload_file(file_path, '<Bucket_Name>', 'YourFileName')
+
 
 # #### Download a file
-s3.download_file('sgsanalytics', 'Stakeholders.xlsx', 'Stakeholders_downloaded_version.xlsx')
+
+# In[ ]:
+
+
+s3.download_file('<Bucket_Name>', '<Bucket_Object_Name>', 'Local_File_Name')
+
 
 # #### Delete a file from a bucket
-s3.delete_object(Bucket='sgsanalytics', Key='Stakeholders.xlsx')
+
+# In[ ]:
+
+
+s3.delete_object(Bucket='<Bucket_Name>', Key='object_name')
+
 
 # #### Delete a bucket
-s3.delete_bucket(Bucket='sgsanalytics')
+
+# In[ ]:
+
+
+s3.delete_bucket(Bucket='<Bucket_Name>')
 
